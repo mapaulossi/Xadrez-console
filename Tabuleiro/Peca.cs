35a7e6593ae8726c795462ba8e929b1931b39423
Matheus Paulossi <mt_paulossi@hotmail.com>
2019-06-21 16:11:01 -0300

    Comecando a implementar o tabuleiro

---
+++ Tabuleiro/Peca.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Tabuleiro
+{
+    class Peca
+    {
+        public Posicao posicao { get; set; }
+        public Cor Cor { get; protected set; }
+
+        public int QteMovimentos { get; protected set; }
+
+        public Tabuleiro Tab { get; set; }
+
+        public Peca(Posicao posicao, Cor cor, Tabuleiro tab)
+        {
+            this.posicao = posicao;
+            Cor = cor;
+            Tab = tab;
+            QteMovimentos = 0;
+        }
+    }
+}
